5269ee7d4efcd0e3195d00c7a09d3088f483dfd4
devjpedro <dev.jpedro@gmail.com>
2023-02-15 17:17:16 -0300

    resolving auth error

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,5 +1,10 @@
 import axios from "axios";
+import { parseCookies } from "nookies";
+
+const cookies = parseCookies();
+const token = cookies.access_token;
 
 export const api = axios.create({
   baseURL: "http://localhost:8000",
+  headers: { Authorization: `Bearer ${token}` },
 });
