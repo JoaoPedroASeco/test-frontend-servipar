c1572de0fd1c811e2b65e1bab86df8e130cdb1e7
devjpedro <dev.jpedro@gmail.com>
2023-02-15 17:51:30 -0300

    adicionando variaveis de ambiente

--- src/services/api.ts
+++ src/services/api.ts
@@ -4,7 +4,11 @@ import { parseCookies } from "nookies";
 const cookies = parseCookies();
 const token = cookies.access_token;
 
+const apiUrl = process.env.API_URL
+  ? process.env.API_URL
+  : "http://localhost:8000";
+
 export const api = axios.create({
-  baseURL: "http://localhost:8000",
+  baseURL: `${apiUrl}`,
   headers: { Authorization: `Bearer ${token}` },
 });
