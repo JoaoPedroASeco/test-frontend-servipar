e6472ea8da2406369801462cd4a7a02547ca8141
devjpedro <dev.jpedro@gmail.com>
2023-02-15 17:59:16 -0300

    adicionando variaveis de ambiente

--- src/services/api.ts
+++ src/services/api.ts
@@ -4,11 +4,7 @@ import { parseCookies } from "nookies";
 const cookies = parseCookies();
 const token = cookies.access_token;
 
-const apiUrl = process.env.API_URL
-  ? process.env.API_URL
-  : "http://localhost:8000";
-
 export const api = axios.create({
-  baseURL: `${apiUrl}`,
+  baseURL: "https://test-backend-servipar-production.up.railway.app",
   headers: { Authorization: `Bearer ${token}` },
 });
